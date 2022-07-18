7d3f7854e66eadb45f572449d67d813b4f0a3138
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-07-18 15:23:59 +0545

    feat: toggle feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Note from "./components/Note";
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [note, setNote] = useState("Type a note");
+  const [toggle, setToggle] = useState(true);
 
-function App() {
+  const handleChange = (event) => {
+    event.preventDefault();
+    console.dir(event);
+    const newObj = {
+      id: props.notes.length + 1,
+      content: note,
+      Date: new Date().toISOString(),
+      important: Math.random() > 0.5 ? true : false,
+    };
+    setNotes([...notes, newObj]);
+    setNote("");
+  };
+
+  const changeNote = (event) => {
+    setNote(event.target.value);
+  };
+
+  const changeToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const usingFilter = () => {
+    let arr = notes.filter((Element) => Element.important === true);
+    return arr;
+  };
+  const notesToShow = toggle ? notes : usingFilter();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <button onClick={changeToggle}>
+        Show {toggle ? "all" : "important"}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} content={note.content} />
+        ))}
+      </ul>
+      <form onSubmit={handleChange}>
+        <input value={note} onChange={changeNote} />
+        <button>CLick me for submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,5 @@
+const Note = (props) => {
+  return <li>{props.content}</li>;
+};
+
+export default Note;
