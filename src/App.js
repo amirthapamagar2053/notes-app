1268672aa3d571e61346114e96433df3a662807b
Amir thapa magar <amirthapa2019@gmail.com>
2022-07-22 11:22:16 +0545

    feat: add the post method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from "axios";
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [note, setNote] = useState("Type a note");
@@ -9,14 +10,23 @@ const App = (props) => {
     event.preventDefault();
     console.dir(event);
     const newObj = {
-      id: notes.length + 1,
+      // id: notes.length + 1,
       content: note,
       Date: new Date().toISOString(),
       important: Math.random() > 0.5 ? true : false,
     };
-    setNotes([...notes, newObj]);
+    axios.post("http://localhost:3001/notes", newObj).then((response) => {
+      console.log(response);
+      setNotes([...notes, response.data]);
+    });
     setNote("");
   };
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/notes")
+      .then((response) => setNotes(response.data));
+    console.log("axios called", notes);
+  }, [toggle]);
 
   const changeNote = (event) => {
     setNote(event.target.value);
